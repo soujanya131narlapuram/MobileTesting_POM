abf9bc157c6e30bb9330adb480011ad39cf85a1b
soujanya <soujanya131narlapuram@gmail.com>
2023-05-24 22:13:31 +0530

    "first commit mobile app testing"

--- src/main/java/com/appium/pages/WhatsappPage.java
+++ src/main/java/com/appium/pages/WhatsappPage.java
@@ -14,7 +14,7 @@ public class WhatsappPage extends BaseClass {
         Thread.sleep(3000);
         driver.findElement(AppiumBy.id("com.whatsapp:id/eula_accept")).click();
         Thread.sleep(1000);
-        driver.setClipboardText("9381220872");
+        driver.setClipboardText("1980922093");
         driver.findElement(AppiumBy.id("com.whatsapp:id/registration_phone")).sendKeys(driver.getClipboardText());
         Thread.sleep(4000);
         driver.findElement(AppiumBy.id("com.whatsapp:id/registration_submit")).click();
